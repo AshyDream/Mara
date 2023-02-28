ad688af9bc984f5035434dd248d7193e7688bb38
Ashy Dream <petro.mrtnk@hotmail.com>
2023-02-28 21:30:21 +0200

    Added verification and execution of commands entered in the chat

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-const fs = require('fs');
+const {token, prefix} = require('./config.json');
 const Discord = require('discord.js');
-const {BOT_TOKEN} = require('./config.json');
-const {DEFAULT_PREFIX} = require('./config.json');
-const Client = require('./handlers/client.js');
+const client = require('./handlers/client.js');
 
-Client.on('ready', (client) => {
+client.commands = require('./handlers/commandsAdder.js');
+client.slashCommands = require('./handlers/slashCommandsAdder.js');
+
+client.on('messageCreate', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('There was an error trying to execute that command!');
+    }
+});
+
+client.on('ready', (client) => {
     console.log('Bot - online: ' + client.user.tag);
 });
 
-Client.login(BOT_TOKEN);
+client.login(token);
